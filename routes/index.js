64fba2578f8ee9100068e769b9fe66092d348ff4
Nicholas Ericksen <nericksen.student@manhattan.edu>
2016-09-05 18:24:58 -0400

    Update button styles

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/habitat', function(req, res, next) {
+  res.render('habitat', { title: 'Express' });
+});
+
+router.get('/climate', function(req, res, next) {
+  res.render('climate', { title: 'Express' });
+});
+
+router.get('/water', function(req, res, next) {
+  res.render('water', { title: 'Express' });
+});
+
+router.get('/hrecos', function(req, res, next) {
+  res.render('hrecos', { title: 'Express' });
+});
+
 module.exports = router;
