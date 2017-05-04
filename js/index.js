b0e7a004d4c67f8371427d5196185a5cbb3ec21d
hhassan1230 <hhassan@oswego.edu>
2017-05-04 00:18:29 -0400

    adds back button logic

--- js/index.js
+++ js/index.js
@@ -6,8 +6,14 @@ jQuery(document).ready(function() {
     $('.logo').on('click', function() {
         init();
     });
-
+    $('#back-button-container').on('click', function() {
+    	init();
+    	$('#back-button-container').hide();
+    });
     $('#main-content').on('click', '.main-menu ul', function() {
         $('.logo').addClass('small-logo');
+        $('#back-button-container').show();
     });
+		$('#back-button-container').hide();
+
 });
